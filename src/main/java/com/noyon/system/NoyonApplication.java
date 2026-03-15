07e019e222d37755a62f5047a49119b6ca761413
KhRote <ssaricaefe@gmail.com>
2026-03-16 01:10:46 +0300

    Proje iskeleti, Docker veritabanı ve User entity eklendi.

---
+++ src/main/java/com/noyon/system/NoyonApplication.java
@@ -0,0 +1,13 @@
+package com.noyon.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoyonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoyonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/noyon/system/NoyonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noyon.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoyonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
