3bb7e7fd1382658c97435fa155142a2f9cd90530
Ensar5499 <ensarkulaksiz54@gmail.com>
2026-04-28 18:15:31 +0300

    Bugün2 yaptiginiz isin kısa bir özeti

---
+++ src/main/java/com/noyon/system/repository/SubscriptionRepository.java
@@ -0,0 +1,9 @@
+package com.noyon.system.repository;
+
+import com.noyon.system.entity.Subscription;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface SubscriptionRepository extends JpaRepository<Subscription, Long> {
+    List<Subscription> findByUserId(Long userId);
+}
