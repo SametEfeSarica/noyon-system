44956d95b28b4e44ace0c5a4a27aa8d2ae94e230
Ensar5499 <ensarkulaksiz54@gmail.com>
2026-05-05 18:48:46 +0300

    Kanban Board surukle-birak, kart ekleme mantigi ve backend gorev kayit servisi eklendi

---
+++ src/main/java/com/noyon/system/controller/ProjectTaskController.java
@@ -0,0 +1,26 @@
+package com.noyon.system.controller;
+
+import com.noyon.system.entity.ProjectTask;
+import com.noyon.system.service.ProjectTaskService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/tasks")
+@RequiredArgsConstructor
+@CrossOrigin(origins = "*") // Geliştirme aşamasında tüm isteklere izin verir
+public class ProjectTaskController {
+
+    private final ProjectTaskService projectTaskService;
+
+    @PostMapping("/add/{userId}/{columnId}")
+    public ResponseEntity<ProjectTask> addTask(
+            @PathVariable Long userId,
+            @PathVariable Long columnId,
+            @RequestBody ProjectTask task) {
+
+        ProjectTask savedTask = projectTaskService.addTask(userId, columnId, task);
+        return ResponseEntity.ok(savedTask);
+    }
+}
